1867044c3aa369f630e8f50e4b532b35ae0ea54f
manjaro linux <tichydominik451@gmail.com>
2020-12-09 21:54:03 +0100

    comments

--- 9.py
+++ 9.py
@@ -1,7 +1,12 @@
-from copy import deepcopy
 
 
-def is_sum(n, nums):
+# --- Day 9: Encoding Error ---
+
+
+def is_sum_of_two(n, nums):
+
+    # Probably not the best way to do this
+    # but hey it works
 
     for x in nums:
         for y in nums[1:]:
@@ -13,32 +18,23 @@ def is_sum(n, nums):
 
 def find_invalid(nums, wnd_size):
 
-    # Checks if each number is a sum of some numbers in window
-    # Returns first number that isn't a sum
-
     for i in range(len(nums) - wnd_size):
-        if not is_sum(nums[i + wnd_size], nums[i:i + wnd_size]):
+        if not is_sum_of_two(nums[i + wnd_size], nums[i:i + wnd_size]):
             return nums[i + wnd_size]
 
 
 def find_sum(nums, invalid):
 
     for i in range(len(nums) - 1):
-        accu = nums[i]
+        s = nums[i]
 
-        for x in range(i+1, len(nums)):
-            accu += nums[x]
+        for x in range(i + 1, len(nums)):
+            s += nums[x]
 
-            if accu > invalid:
+            if s > invalid:
                 break
-            
-            if accu == invalid:
+            if s == invalid:
                 return (i, x)
-
-
-def parse(raw):
-
-    return [int(x) for x in raw.split('\n')[:-1]]
 
 
 def main():
@@ -46,16 +42,13 @@ def main():
     with open('input.txt', 'r') as f:
         raw = f.read()
 
-    nums = parse(raw)
+    nums = [int(x) for x in raw.split('\n')[:-1]]
 
-    wnd_size = 25
-    invalid_num = find_invalid(nums, wnd_size)
+    invalid_num = find_invalid(nums, 25)
     x, y = find_sum(nums, invalid_num)
-    
-    min_value = min(nums[x:y+1])
-    max_value = max(nums[x:y+1])
 
-    print(min_value + max_value)
+    print("--- Part One ---\n", invalid_num)
+    print("--- Part Two ---\n", min(nums[x:y+1]) + max(nums[x:y+1]))
 
 
 main()
