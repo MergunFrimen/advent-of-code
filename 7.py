f1779800384e5c6f7c14363d61f645e07152fee7
manjaro linux <tichydominik451@gmail.com>
2020-12-10 13:00:52 +0100

    Day 10

--- 7.py
+++ 7.py
@@ -1,81 +1,91 @@
+from typing import Dict, List, Tuple
 import re
-from typing import Dict, List
 
 
 class Bag:
 
-    def __init__(self, name, bags):
+    def __init__(self, name: str, bags: List['Bag']):
         self.name = name
         self.bags = bags
 
 
-def parse(raw):
+def parse(raw: str) -> Dict[str, Bag]:
 
-    bags = dict()
     regex = r"([a-z ]+) bags contain (.+)."
+    bags = dict()
 
     # Initial dictionary creation
     for bag, contents in re.findall(regex, raw):
-        bags_list = [(x.split()[0], " ".join(x.split()[1:-1])) for x in contents.split(', ') if x != "no other bags"]
-        bags[bag] = Bag(bag, bags_list)
+        bags[bag] = Bag(bag, fill_bag(contents))
 
     # Connects the bags
     for bag in bags.values():
         bag.bags = [(int(x[0]), bags[x[1]]) for x in bag.bags]
 
-
     return bags
 
 
-def search(bag, name, amount, level=0):
+def fill_bag(contents: List[str]) -> List[Tuple[int, str]]:
 
-    if level != 0 and bag.name == name:
+    # Skip these
+    if contents == "no other bags":
+        return []
+
+    bag_list = []
+
+    # Create tuples for bag list
+    for x in contents.split(', '):
+        amount = int(x.split()[0])
+        bag_name = " ".join(x.split()[1:-1])
+        bag_list.append((amount, bag_name))
+
+    return bag_list
+
+
+
+def search(bag: Bag, amount: int, level: int=0):
+
+    if level != 0 and bag.name == "shiny gold":
         return True
 
-    print("  " * level, amount, bag.name)
+    # print("  " * level, amount, bag.name)
 
     for n, x in bag.bags:
-        if search(x, name, n, level + 1):
+        if search(x, n, level + 1):
             return True
 
     return False
 
 
-def count_bags(bag, amount, lst, level=0):
+def count_bags(bag, amount, level=0):
 
     counter = 1
 
     for n, x in bag.bags:
-        counter += n * count_bags(x, n, lst, level + 1)
+        counter += n * count_bags(x, n, level + 1)
 
     return counter
 
 
-def part1():
+def main():
 
     with open('input.txt', 'r') as f:
         raw: str = f.read()
+    
+    # {bagcolor: Bag} dictionary
+    bags = parse(raw)
 
-    bags: Dict[str, Bag] = parse(raw)
-    counter = 0
+    shiny_bag_counter = 0
+    bag_req_counter = 0
 
     for bag in bags.values():
-        if search(bag, "shiny gold", 1):
-            counter += 1
+        if search(bag, 1):
+            shiny_bag_counter += 1
 
-    print(counter)
+    bag_req_counter = count_bags(bags["shiny gold"], 1)
 
-
-def part2():
-
-    with open('input.txt', 'r') as f:
-        raw: str = f.read()
-
-    bags: Dict[str, Bag] = parse(raw)
-    amounts: Dict[str]
-
-    lst = []
-    print(count_bags(bags["shiny gold"], 1, lst))
+    print("--- Part One ---\n", shiny_bag_counter)
+    print("--- Part Two ---\n", bag_req_counter)
 
 
-part2()
+main()
