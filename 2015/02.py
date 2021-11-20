bf35494fcf1fd7177d98c91ba662fcad8e8488c5
Dominik Tichy <492772@mail.muni.cz>
2021-11-20 21:06:02 +0100

    Day 2

---
+++ 2015/02.py
@@ -0,0 +1,33 @@
+from functools import reduce
+import operator
+
+
+def wrapping(l):
+    l1 = min(l)
+    l.remove(l1)
+    l2 = min(l)
+    return l1, l2
+
+
+def ribbon(l):
+    x, y = wrapping(l[::])
+    return 2*x + 2*y + reduce(operator.mul, l)
+
+
+# PART 1
+def part1():
+    with open("input.txt", "r") as f:
+        for l, w, h in [map(int, i.split('x')) for i in f.read().split('\n')[:-1]]:
+            yield 2*l*w + 2*w*h + 2*h*l + operator.mul(*wrapping([l, w, h]))
+
+
+# PART 2
+def part2():
+    with open("input.txt", "r") as f:
+        for l, w, h in [map(int, i.split('x')) for i in f.read().split('\n')[:-1]]:
+            yield ribbon([l, w, h])
+
+
+# MAIN
+print(sum(part1()))
+print(sum(part2()))
