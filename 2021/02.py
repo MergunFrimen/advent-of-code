a5b15fc629b11805fa2ddfb5645b900ea7956941
Dominik Tichy <492772@mail.muni.cz>
2021-12-03 15:56:08 +0100

    Day 3

---
+++ 2021/02.py
@@ -0,0 +1,32 @@
+def read():
+    with open("input.txt") as f:
+        return [x.split() for x in f.read().split('\n')[:-1]]
+
+
+def part1(m):
+    r = [0, 0]
+    for x, y in m:
+        if x == 'forward':
+            r[0] += int(y)
+        elif x == 'up':
+            r[1] -= int(y)
+        elif x == 'down':
+            r[1] += int(y)
+    return r[0] * r[1]
+
+
+def part2(m):
+    r = [0, 0, 0]
+    for x, y in m:
+        if x == 'forward':
+            r[0] += int(y)
+            r[1] += r[2] * int(y)
+        elif x == 'up':
+            r[2] -= int(y)
+        elif x == 'down':
+            r[2] += int(y)
+    return r[0] * r[1]
+
+
+print(part1(read()))
+print(part2(read()))
---
+++ 2021/03.py
@@ -0,0 +1,52 @@
+def read():
+    with open("input.txt") as f:
+        return f.read().split()
+
+
+def part1(m):
+    x = [0 for _ in range(len(m[0]))]
+    for w in m:
+        for i in range(len(w)):
+            if w[i] == '1':
+                x[i] += 1
+            else:
+                x[i] -= 1
+    g = int(''.join(map(lambda i : '1' if i > 0 else '0', x)), 2)
+    e = int(''.join(map(lambda i : '1' if i < 0 else '0', x)), 2)
+    return g * e
+
+
+def part2(m):
+    m = [[int(b) for b in w] for w in m]
+    o = [0 for _ in range(len(m[0]))]
+    s = [0 for _ in range(len(m[0]))]
+
+    for i in range(len(m[0])):
+        wo = list(filter(lambda a : a[:i] == o[:i], m))
+        if len(wo) == 1:
+            o = wo[0]
+            break
+        for w in wo:
+            if w[i] == 1:
+                o[i] += 1
+            else:
+                o[i] -= 1
+        o[i] = 1 if o[i] >= 0 else 0
+
+    for i in range(len(m[0])):
+        so = list(filter(lambda a : a[:i] == s[:i], m))
+        if len(so) == 1:
+            s = so[0]
+            break
+        for w in so:
+            if w[i] == 1:
+                s[i] += 1
+            else:
+                s[i] -= 1
+        s[i] = 0 if s[i] >= 0 else 1
+
+    return int(''.join(map(str, o)), 2) * int(''.join(map(str, s)), 2)
+
+
+print(part1(read()))
+print(part2(read()))
