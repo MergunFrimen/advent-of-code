7a7030afdfaaa05b36f858e1523333d3e05193d8
Dominik Tichy <492772@mail.muni.cz>
2021-12-14 14:00:35 +0100

    day 14 - hardest yet

--- 2021/14.py
+++ 2021/14.py
@@ -6,19 +6,29 @@ def read():
         m = f.read().split("\n\n")
     return m[0], {k:v for k,v in re.findall("(\S+) -> (\S+)", m[1])}
 
-def part1():
+def part(max_depth):
     template, rules = read()
-    for _ in range(10):
-        new = [] 
-        for i in range(len(template) - 1):
-            k = template[i:i+2]
-            new += [template[i], rules[k]]
-        template = ''.join(new + [template[-1]])
-    c = Counter(template).values()
-    print(max(c) - min(c))
+    template = [template[i:i+2] for i in range(len(template) - 1)]
+    count = {k:0 for k in set(rules.values())}
 
-def part2():
-    pass
+    i = -1
+    for x in template:
+        i = rec(rules, x, max_depth, 0, i + 1, count)
+        print(i)
 
-part1()
-part2()
+    print(count)
+
+def rec(rules, x, max_depth, depth, i, count):
+    if depth == max_depth:
+        count[x[0]] += 1
+        if i == 3 * sum(2**i for i in range(max_depth + 1)) - 1:
+            count[x[1]] += 1
+        return i
+    
+    new = rules[x]
+    left, right = x[0] + new, new + x[1]
+
+    i = rec(rules, left, max_depth, depth + 1, i + 1, count)
+    return rec(rules, right, max_depth, depth + 1, i + 1, count)
+
+part(2)
