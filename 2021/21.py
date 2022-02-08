e777c2eb24ec4f6e0fd58c1f9e9a9b3c6b49079c
Dominik Tichy <492772@mail.muni.cz>
2022-02-08 16:27:40 +0100

    small changes to README

--- 2021/21.py
+++ 2021/21.py
@@ -1,14 +1,5 @@
 import re
-
-dim = {
-       3 : 1,
-       4 : 3,
-       5 : 6,
-       6 : 7,
-       7 : 6,
-       8 : 3,
-       9 : 1 
-       }
+from collections import defaultdict
 
 def read():
     with open("input/21.txt") as f:
@@ -24,36 +15,46 @@ def part1():
         i += 1
     print(3 * i * min(scores))
 
-def move_player(players, scores, i):
-    players[i % 2] += sum(range(3*i + 1, 3*i + 4))
-    scores[i % 2] += players[i % 2] % 10 + 1
-    return i + 1
+dim = {
+       3 : 1,
+       4 : 3,
+       5 : 6,
+       6 : 7,
+       7 : 6,
+       8 : 3,
+       9 : 1
+       }
+
+def move(position, s):
+    position += s
+    if position > 10:
+        return position - 10
+    return position
+
+def count(player_score, position, l, steps, possibility_amount, total):
+    if total >= 21:
+        player_score[steps] += possibility_amount
+        return
+    for s, o in dim.items():
+        new_position = move(position, s)
+        count(player_score, new_position, l, steps + 1, possibility_amount * o, total + new_position)
+
+def compare(p1, p2):
+    wins = [0, 0]
+    for k1, v1 in p1.items():
+        for k2, v2 in p2.items():
+            if k1 <= k2:
+                wins[0] += v1 * v2
+            if k1 > k2:
+                wins[1] += v1 * v2
+    print(wins)
 
 def part2():
-    p1, p2 = read()
-    players, scores, m = (p1 - 1, p2 - 1), (0, 0), [0, 0]
-    rec(players, scores, m, 1, 0)
-    print(m)
-
-def rec(p, s, m, t, i):
-    p1, p2 = p
-    s1, s2 = s
-
-    if s1 >= 21:
-        m[0] += t
-        return
-    elif s2 >= 21:
-        m[1] += t
-        return
-
-    for x in range(3, 10):
-        if i % 2 == 0:
-            new_p = (p1 + x, p2)
-            new_s = (s1 + new_p[0] % 10 + 1, s2)
-        else:
-            new_p = (p1, p2 + x)
-            new_s = (s1, s2 + new_p[1] % 10 + 1)
-        rec(new_p, new_s, m, t * dim[x], i + 1)
+    s1, s2 = read()
+    p1, p2 = defaultdict(int), defaultdict(int)
+    count(p1, s1, [], 0, 1, 0)
+    count(p2, s2, [], 0, 1, 0)
+    compare(p1, p2)
 
 part1()
 part2()
--- 2021/README.md
+++ 2021/README.md
@@ -2,9 +2,7 @@
 
 ## TODO
 
-- Day 16: Packet Decoder
 - Day 17: Trick Shot
-- Day 18: Snailfish
 - Day 19: Beacon Scanner
 - Day 20: Trench Map
 - Day 21: Dirac Dice
