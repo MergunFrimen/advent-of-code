6192594703400d9a12bd3d0f5a2deeb1439178da
Dominik Tichy <tichydominik451@gmail.com>
2022-12-07 16:09:50 +0100

    day 7 done

--- 2022/07/07.py
+++ 2022/07/07.py
@@ -1,16 +1,58 @@
+import re
 
 
 def get_data():
     with open('input.txt') as f:
-        pass
+        return re.findall('\$ \w+ *\S*|dir \w+|\d+ \S+', f.read())
+
+
+def create_metadata(data):
+    cwd = ''
+    metadata = {}
+    ls = False
+    for line in data[1:]:
+        if line[0] == '$' and line[2:4] == 'cd':
+            _, _, dirname = line.split()
+            if dirname == '..':
+                cwd = '/'.join(cwd.split('/')[:-1])
+            else:
+                cwd += '/' + dirname
+            ls = False
+        if line[0] == '$' and line[2:4] == 'ls':
+            ls = True
+            continue
+        if ls:
+            info, name = line.split()
+            path = cwd + '/' + name
+            metadata[path] = info
+    return metadata
+
+
+def get_dir_sizes(data):
+    metadata = create_metadata(data)
+    dir_sizes = dict([('/', 0)] + [(path, 0) for path, info in metadata.items() if info == 'dir'])
+    
+    for filepath, size in metadata.items():
+        if size == 'dir':
+            continue
+        size = int(size)
+        dir_sizes['/'] += size
+        for i in range(1, filepath.count('/')):
+            dirname = '/'.join(filepath.split('/')[:-i])
+            dir_sizes[dirname] += size
+    
+    return dir_sizes
 
 
 def part1(data):
-    pass
+    dir_sizes = get_dir_sizes(data)
+    return sum(filter(lambda x: int(x) <= 10**5, dir_sizes.values()))
 
 
 def part2(data):
-    pass
+    dir_sizes = get_dir_sizes(data)
+    needed = 30000000 - (70000000 - dir_sizes['/'])
+    return sorted(filter(lambda x: int(x) >= needed, dir_sizes.values()))[0]
 
 
 def main():
