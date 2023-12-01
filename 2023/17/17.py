0a425e81f24b894c26c4eac5141c6e292e5b552b
Dominik Tichy <tichydominik451@gmail.com>
2023-12-01 17:55:59 +0100

    2023 init

---
+++ 2023/17/17.py
@@ -0,0 +1,21 @@
+# from _ import _
+
+
+def get_data():
+    with open('input.txt') as f:
+        pass
+
+
+def part1(data):
+    pass
+
+
+def part2(data):
+    pass
+
+
+def main():
+    print(f'part1: {part1(get_data())}')
+    print(f'part2: {part2(get_data())}')
+
+main()
